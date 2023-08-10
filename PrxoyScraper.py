3055a6afce8101e9ae7d4c372a91985aaaab82ff
TheShelbyOne <98327786+Shelby-28@users.noreply.github.com>
2023-08-10 21:01:18 +0300

    Update PrxoyScraper.py

--- PrxoyScraper.py
+++ PrxoyScraper.py
@@ -1,25 +1,22 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 
-# URL to retrieve the text from
 url = "https://free-proxy-list.net/"
+cwd = os.getcwd()
+
+# ANSI COLORS
+RED  = '\033[1;31m' 
+GREEN  = '\033[1;32m'
+CLOSE = '\x1b[0m'
 
 try:
-    # Send a GET request to the URL
     response = requests.get(url)
-
-    # Check if the request was successful
     response.raise_for_status()
-
-    # Parse the HTML content
     soup = BeautifulSoup(response.text, "html.parser")
-
-    # Find all table rows containing proxy information
     rows = soup.find_all("tr")
 
-    # Check if rows are found
     if len(rows) > 0:
-        # Extract the proxy information from the rows
         proxies = []
         for row in rows:
             columns = row.find_all("td")
@@ -28,18 +25,17 @@ try:
                 port = columns[1].get_text()
                 proxies.append(f"{ip}:{port}")
 
-        # Remove unwanted lines starting with "Total:" and country statistics
         proxies = [proxy for proxy in proxies if not proxy.startswith(("Total:", " US:", " IN:", " KH:", " BD:", " JP:", " VE:", " DO:"))]
 
-        # Save the proxy information to a file
-        with open("proxy.txt", "w") as file:
+        with open(f"{cwd}/Files/proxys.txt", "w") as file:
             file.write("\n".join(proxies))
 
-        print("\n[+]Proxy information saved to 'proxy.txt' file.\n")
+        print("\n{GREEN}[+] Proxys saved in /Files/proxys.txt {CLOSE}\n")
     else:
-        raise Exception("[-] No table rows found containing proxy information.")
+        raise Exception("{RED}[-] No table rows found containing proxy information {CLOSE}")
 
 except requests.exceptions.RequestException as e:
-    print(f"[-] Error: Failed to retrieve text from {url}. Exception: {e}")
+    print(f"{RED}[-] Error{CLOSE}: Failed to retrieve text from {url}. Exception: {e}")
 except Exception as e:
-    print(f"[-] Error: {e}")
+    print(f"{RED}[-] Error{CLOSE}: {e}")
+    
